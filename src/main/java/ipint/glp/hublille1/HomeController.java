72c3654447478a95fb60938bf7b70853f1d44923
Soukaina Bekkai <bekkai.soukaina@gmail.com>
2014-12-05 11:13:13 +0100

    test sb

--- src/main/java/ipint/glp/hublille1/HomeController.java
+++ src/main/java/ipint/glp/hublille1/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page. Nouveau commit (gbergeus, Clément)
+ * test soukaina
  */
 @Controller
 public class HomeController {
